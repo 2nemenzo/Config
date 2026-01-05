d9a1f35b5c7097fd5eee1008e0d037a7bf0388ac
2nemenzo <email>
2026-01-04 17:05:17 -0800

    nixos configuration files

---
+++ nixos/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Hyprland on NixOS";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.nat = import ./home.nix;
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ nixos/home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "nat";
+  home.homeDirectory = "/home/nat";
+  home.stateVersion = "25.05";
+  fonts.fontconfig.enable = true;
+
+  home.packages = with pkgs; [
+    fontconfig
+    nerd-fonts.jetbrains-mono
+  ];
+
+  programs.bash = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "2nemenzo";
+    userEmail = "email";
+  };
+
+  home.file.".config/hypr".source = ./config/hypr;
+  home.file.".config/waybar".source = ./config/waybar;
+  home.file.".config/foot".source = ./config/foot;
+}
