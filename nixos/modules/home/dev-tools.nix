c10ec20a9c3b470cae7be9a221def0d61d3e9687
2nemenzo <47703615+2nemenzo@users.noreply.github.com>
2026-01-26 23:07:31 -0800

    add cargo bins to path

--- nixos/modules/home/dev-tools.nix
+++ nixos/modules/home/dev-tools.nix
@@ -1,13 +1,17 @@
 { ... }:
 
 {
-  flake.modules.homeManager.dev-tools = { pkgs, ... }: {
+  flake.modules.homeManager.dev-tools = { pkgs, config, ... }: {
     home.packages = with pkgs; [
       ripgrep
       fd
       unzip
       cargo
       rustc
+    ];
+
+    home.sessionPath = [
+      "${config.home.homeDirectory}/.cargo/bin"
     ];
   };
 }
